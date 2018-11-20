6825462a13a115c5897bebee2f9c4cb56b7a813e
Zaustuv <modebence@gmail.com>
2018-11-20 20:33:46 +0100

    game description refreshed

--- ascii_rougelike/main.cpp
+++ ascii_rougelike/main.cpp
@@ -2,22 +2,22 @@
     Ascii Rougelike game by Zaustuv (aka Niznize), 2018
     License according to GNU GPL
 
-    This game is a text-based command line rougelike game, with customizable levels, adjustable difficulty and load/save funcions.
-    Each level is built up on a <> x <> map, from characters having the following meaning:
+    This game is a text-based command line rougelike game, with 9 levels, adjustable difficulty and load/save funcions.
+    Each level is built up on a customizable sized map, from characters having the following meaning:
 
     @: Player. You can move up/down/left/right by pressing one of the WSAD buttons at once. You cannot walk through walls, enemies, shops
-        or exits, but you can press 'E' to enter the latter two, or <spacebar> to attack the enemy. You can also have different wearpons
-        which you can change in the menu.
+        or exits. You can also use the WSAD buttons to attack an enemy in front of you. You can also have different wearpons or shields,
+        but only one of each equipped. You can change this item in the player menu.
     S: Enemy. They move in a random fashion, and can attack 1 tile away from you.
-    Z: Super enemy. They have more life, and can attack you from 2 tiles away.
     M: Shop. You can buy many useful items here.
-    Q: savestate.
+    Q: Safe house.
     #: Wall. You cannot walk through this place.
     H: Door: Press 'E' in front of it to enter to the next level!
 
     Other important notes:
     - Enemies do not move until you move.
-    - You can get money and XP after defeating enemies. after reaching enough XP, you can level up!
+    - You can get money and XP after defeating enemies. after reaching enough XP, you can level up! Level up boost the stats of your equipped
+    wearpon
 */
 
 
